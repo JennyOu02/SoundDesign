c79a914c753b713203d790e997b7e89bed95902d
Jenny Ou <95497154+JennyOu02@users.noreply.github.com>
2023-02-27 16:27:41 +0000

    Add files via upload

--- 1 Noise worklet/basicnoise.js
+++ 1 Noise worklet/basicnoise.js
@@ -2,9 +2,8 @@ registerProcessor('noise-generator',class extends AudioWorkletProcessor {
   
   process(inputs, outputs) {
 
-    
 
-//     for (let i=0;i<outputs[0][0].length;++i) outputs[0][0][i]=2*Math.random()-1
+
     for (let i=0;i<outputs[0][1].length;++i) outputs[0][1][i]=2*Math.random()-1
     return true
   }
